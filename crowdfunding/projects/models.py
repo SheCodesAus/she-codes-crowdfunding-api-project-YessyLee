677d880e4731b5b7885740e8ac95ab7e073e8dd7
Yessy Lee <107485501+YessyLee@users.noreply.github.com>
2023-01-14 12:30:26 +0800

    project setup completed class 2

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField() #to check if the project is open
+    date_created = models.DateTimeField(auto_now_add=True) #auto now add, add date time
+    owner = models.CharField(max_length=200)
+    
---
+++ crowdfunding/projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('projects/', views.ProjectList.as_view(), name="project-list"),#always include name on urls
+    path('projects/<int:pk>/', views.ProjectDetail.as_view(), name="project-detail"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns) #function that take urls whether it take json or other file, rpobably will be deleted
