db82147aa3bb192c3671cccfabc4e099e1443dd6
Yessy Lee <107485501+YessyLee@users.noreply.github.com>
2023-01-19 21:21:43 +0800

    changes from Tuesday class 17/01/23

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+User = get_user_model() #get from setting.py
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -9,11 +11,19 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField() #to check if the project is open
     date_created = models.DateTimeField(auto_now_add=True) #auto now add, add date time
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="owner_projects" #related_name link backward
+        )
     
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='pledges')
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="supporter_pledges"
+    )
--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -14,10 +14,22 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
+<<<<<<< Updated upstream
     owner = serializers.CharField(max_length=200) #removing owner??
+=======
+    owner = serializers.ReadOnlyField(source="owner_id") #call owner id
+>>>>>>> Stashed changes
     
     def create(self, validated_data): #validated data is the dictionary function
         return Project.objects.create(**validated_data) #asterisk is to return the pair key value
     
+<<<<<<< Updated upstream
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
+=======
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta: #define how the model form work
+        model = Pledge
+        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+        read_only_fields = ['id', 'supporter']
+>>>>>>> Stashed changes
