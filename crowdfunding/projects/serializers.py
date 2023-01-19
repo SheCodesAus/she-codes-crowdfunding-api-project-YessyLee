9c4caac3b8eb6b3d900d3cc967c5065d79b15a8b
Yessy Lee <107485501+YessyLee@users.noreply.github.com>
2023-01-19 21:15:20 +0800

    Changes from homework from Sat 14/01

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from .models import Project, Pledge
 
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta: #define how the model form work
+        model = Pledge
+        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+        
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
     title = serializers.CharField(max_length=200)
@@ -9,12 +14,10 @@ class ProjectSerializer(serializers.Serializer):
     image = serializers.URLField()
     is_open = serializers.BooleanField()
     date_created = serializers.DateTimeField()
-    owner = serializers.CharField(max_length=200)
+    owner = serializers.CharField(max_length=200) #removing owner??
     
     def create(self, validated_data): #validated data is the dictionary function
         return Project.objects.create(**validated_data) #asterisk is to return the pair key value
     
-class PledgeSerializer(serializers.ModelSerializer):
-    class Meta: #define how the model form work
-        model = Pledge
-        fields = ['id', 'amount', 'comment', 'anonymous', 'project', 'supporter']
+class ProjectDetailSerializer(ProjectSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
